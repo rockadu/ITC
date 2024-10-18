fc368db54f3827ace0e3f8bf250c1978229a8fb3
Carlos Assis <carlos.assis@escriba.com.br>
2024-10-17 22:01:33 -0300

    Inicio das telas de Setor, Cargos e Unidades mudança para o menu ser partial

---
+++ App/Controllers/OrganizacaoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class OrganizacaoController : Controller
+    {
+        public IActionResult Setores()
+        {
+            return View();
+        }
+
+        public IActionResult Cargos()
+        {
+            return View();
+        }
+
+        public IActionResult Unidades()
+        {
+            return View();
+        }
+    }
+}
